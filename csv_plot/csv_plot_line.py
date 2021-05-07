163fcfac5b99420f92aad4ceb4dbee211163253b
maundersea <m.na.akei+github@gmail.com>
2021-05-07 11:11:37 +0900

    [update] add rangeslider for x-axis, but rangeslider and plotly.express are not copatible.

--- csv_plot/csv_plot_line.py
+++ csv_plot/csv_plot_line.py
@@ -81,6 +81,7 @@ example:
     arg_parser.add_argument("--yrange", dest="YRANGE", help="range of y", type=str, metavar='YMIN,YMAX')
     arg_parser.add_argument("--log_x", dest="LOG_X", help="log-scaled x axis", action="store_true", default=False)
     arg_parser.add_argument("--log_y", dest="LOG_Y", help="log-scaled y axis", action="store_true", default=False)
+    arg_parser.add_argument("--rangeslider", dest="RANGESLIDER", help="show range slider of x", action="store_true")
     arg_parser.add_argument("--noautoscale", dest="NOAUTOSCALE", help="not autoscale x or y for facets", action="store_false")
     arg_parser.add_argument("--error_x", dest="ERRORX", help="column name of error x", type=str, metavar='COLUMN')
     arg_parser.add_argument("--error_y", dest="ERRORY", help="column name of error y", type=str, metavar='COLUMN')
@@ -123,6 +124,7 @@ if __name__ == "__main__":
     no_auto_scale = args.NOAUTOSCALE
     error_x = args.ERRORX
     error_y = args.ERRORY
+    x_rangeslider = args.RANGESLIDER
     # spline_mode = args.SPLINE
 
     if output_file is None:
@@ -283,6 +285,9 @@ output: {}
             fig.update_yaxes(matches=None)
         elif not no_auto_scale and (row_facet is None or len(row_facet) > 0) and (col_facet is not None and len(col_facet) > 0):
             fig.update_xaxes(matches=None)
+
+    if x_rangeslider:
+        fig.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
 
     if output_format == "json":
         if output_file == sys.stdout.buffer:
