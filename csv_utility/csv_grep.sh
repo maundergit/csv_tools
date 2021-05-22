7a8130f2398259fdd621847a07c109894776a6ee
maundersea <m.na.akei+github@gmail.com>
2021-05-22 14:24:01 +0900

    [update] add mulitpattern mode and xsv mode

--- csv_utility/csv_grep.sh
+++ csv_utility/csv_grep.sh
@@ -53,20 +53,31 @@ check_commands csvgrep
 usage_exit() {
     cat <<EOF 2>&1
 grep for csv with regex
-Usage: $SNAME [-c columns] [-i] pattern csv_file
+Usage: $SNAME [-c columns] [-i] [-m] patterns csv_file
 options:
   -c columns: columns where patternwas searched. 
   -i        : invert match
+  -m        : multipattern mode
+
+remark:
+  with '-m', some patterns with csv format are available. then results for each pattern will be merged.
+
+example:
+  csv_grep.sh -c COL_0006 "PAT\d+3" big_sample_arb.csv |csvcut -c COL_0006 | less
+  csv_grep.sh -c COL_0006 -m "PAT\d+3,PAT\d+4" big_sample_arb.csv |csvcut -c COL_0006 | less
+
 EOF
     exit 1
 }
 
-while getopts ic:h OPT
+while getopts ic:mh OPT
 do
     case $OPT in
         i)  INVERT=1
             ;;
         c)  COLUMNS=$OPTARG
+            ;;
+        m)  MULTI_PATTERNS=1
             ;;
         h)  usage_exit
             ;;
@@ -90,21 +101,59 @@ INPUT=$(check_and_get_stdin "${INPUT}")
 G_OPTS=
 if [ "${INVERT}" != "" ]; then
     G_OPTS="--invert-match"
+    echo "-- invert match mode" 1>&2
 fi
 
-if [ "${COLUMNS}" != "" ]; then
-    OIFS=${IFS}
-    IFS=\|
-    CS=($(echo "${COLUMNS}" | sed "s/\r//g" | tr "," "|"))
-    IFS=${OIFS}
-    head -1 ${INPUT}
-    for C in ${CS[@]}; do
-	csvgrep ${G_OPTS} --columns ${C} --regex "${PATTERN}" "${INPUT}" | sed '1d'
-    done | sort | uniq 
+if [ "${MULTI_PATTERNS}" = "" ]; then
+    PATS=(${PATTERN})
+    echo "-- single-pattern mode: ${PATS[@]}" 1>&2
 else
-    head -1 "${INPUT}"
-    grep ${G_OPTS} -P "${PATTERN}" "${INPUT}"
+    PATS=($(echo "${PATTERN}" | csvformat -D" "))
+    echo "-- multi-pattern mode: ${PATS[@]}" 1>&2
 fi
+
+function do_grep(){
+    PATTERN=$1
+    if [ "${COLUMNS}" != "" ]; then
+	OIFS=${IFS}
+	IFS=\|
+	CS=($(echo "${COLUMNS}" | sed "s/\r//g" | tr "," "|"))
+	IFS=${OIFS}
+	for C in ${CS[@]}; do
+	    csvgrep ${G_OPTS} --columns ${C} --regex "${PATTERN}" "${INPUT}" | sed '1d'
+	    csvgrep ${G_OPTS} --columns ${C} --regex "${PATTERN}" "${INPUT}" | sed '1d'
+	done | sort | uniq 
+    else
+	grep ${G_OPTS} -P "${PATTERN}" "${INPUT}"
+    fi
+}
+function do_grep_xsv(){
+    PATTERN=$1
+    if [ "${COLUMNS}" != "" ]; then
+	xsv search --select ${COLUMNS} ${G_OPTS} "${PATTERN}" "${INPUT}" | sed '1d'
+    else
+	xsv search ${G_OPTS} "${PATTERN}" "${INPUT}" | sed '1d'
+    fi
+}
+
+ACOLUMNS=${COLUMNS}
+(head -1 "${INPUT}")
+COLUMNS=${ACOLUMNS}
+
+if [ "$(which xsv)" = "" ]; then
+    GREP_MODE=csvgrep/grep
+else
+    GREP_MODE=xsv
+fi
+echo "-- grep mode: ${GREP_MODE}" 1>&2
+for P in ${PATS[@]}; do
+    echo "-- processing pattern: ${P}" 1>&2
+    if [ "${GREP_MODE}" = "xsv" ]; then
+	do_grep_xsv ${P}
+    else
+	do_grep ${P}
+    fi
+done | sort | uniq
 
 #----
 remove_tmpdir
