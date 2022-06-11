c342a5a439cd5e0e510e0fa101c890bd56b318a3
maundersea <m.na.akei+github@gmail.com>
2022-06-11 14:12:39 +0900

    remove unused code

--- csv_utility/csv_remove_lf.sh
+++ csv_utility/csv_remove_lf.sh
@@ -14,123 +14,8 @@ DDIR_ABS=$(realpath ${DDIR})
 TMPDIR=/tmp
 # TMPDIR=/tmp/${SNAME}.$$
 
-check_os () {
-    OSNAME=$(uname -o 2>/dev/null || echo others)
-    OSVER=$(uname -a 2>/dev/null)   # -r or -v
-    if [ "${OSNAME}" = "GNU/Linux" ]; then
-        if [[ "${OSVER}" =~ microsoft ]]; then
-            echo something to do for WSL
-        else
-            echo something to do for linux other than WSL
-        fi
-        echo something to do for linux
-    else
-        echo something to do for others
-    fi
-}
-check_xwindow() {
-    if [ "$DISPLAY" = "" ]; then
-        echo "-- error: 'DISPLAY' environment was not definded." >&2
-        exit 1
-    fi
-    if [ "$(xset q 2>/dev/null)" = "" ]; then
-        echo "-- error: No X server at \$DISPLAY [$DISPLAY]" >&2
-        exit 1
-    fi
-}
-check_do() {
-    MODE=$1
-    CHK_CMD=$2
-    shift
-    CMD="$@"
-    unset MES
-    if [ "$(which ${CHK_CMD})" = "" ]; then
-        if [ "${MODE}" = "0" ]; then
-            echo "-- warning: not found ${CHK_CMD}"
-        else
-            echo "-- error: not found ${CHK_CMD}"
-            exit 1;
-        fi
-    else
-        ${CMD}
-    fi
-}
-change_stdout(){
-    F=$@
-    exec 1<&-     # close stdout
-    exec 1<>${F}  # reopen stdout as ${F} for read and write
-    # exec 2<&-   # close stderr
-    # exec 2>&1   # reopen 
-}
-check_commands_and_install() {
-    # usage: check_commands_and_install "${array[@]}"
-    CHK_CMDS=("$@")
-    unset MES
-    for c in "${CHK_CMDS[@]}"; do
-        if [ "$(which ${c})" = "" ]; then
-            if [ -x /usr/lib/command-not-found ]; then
-                APT_CMD=$(/usr/lib/command-not-found -- "$c" 2>&1 | awk '/^(sudo|apt)/ {print $0}')
-                ${APT_CMD}
-            elif [ -x /usr/share/command-not-found/command-not-found ]; then
-                APT_CMD=$(/usr/share/command-not-found -- "$c" 2>&1 | awk '/^(sudo|apt)/ {print $0}')
-                ${APT_CMD}
-            else
-                MES="${MES}-- error: not found $c\n"
-            fi
-        fi
-    done
-    if [ "${MES}" != "" ]; then
-        echo -e "${MES}" 1>&2
-        exit 1
-    fi
-}
-get_absolutepath() {
-    P=$1
-    RES=$(cd $(dirname ${P}) && pwd)/$(basename ${P})
-    echo ${RES}
-}
-print_array(){
-    # bash - How to pass an array as function argument? - Ask Ubuntu https://askubuntu.com/questions/674333/how-to-pass-an-array-as-function-argument
-    # usage: print_array "${array[@]}"
-    local A
-    A=("$@")
-    for v in "${A[@]}"; do
-        echo "'$v'"
-    done
-}
-function stderr_out(){
-    MESSAGES=("$@")
-    for m in "${MESSAGES[@]}"; do
-	echo "${m}" 1>&2
-    done
-}
-unused_port(){
-    # usage: unused_port star_port [end_port]
-    SP=$1
-    EP=${2:-$((${SP}+10))}
-    PS=($(nc -v -z 127.0.0.1 ${SP}-${EP} 2>&1 | awk '/failed/ {print $6}'))
-    if (( ${#PS[@]} > 0)); then
-	echo "${PS[0]}"
-    else
-	echo "??error:unused_port: unused port was not found in ${SP}-${EP}" 1>&2
-	exit 1
-    fi
-}
 
 #---- 
-remove_tmpdir(){
-    if [[ "${TMPDIR}" != "" && "${TMPDIR}" =~ ${SNAME} && -e "${TMPDIR}" ]]; then
-	rm -rf "${TMPDIR}"
-    fi
-}
-make_tmpfile(){
-    ID=$1
-    if [[ "${TMPDIR}" != "" && ! -e "${TMPDIR}" ]]; then
-	mkdir "${TMPDIR}"
-    fi
-    FN="${TMPDIR}/${ID}_$$.tmp"
-    echo "${FN}"
-}
 check_and_get_stdin(){
     if [ "${INPUT}" = "-" ]; then
 	if [ ! -e "${TMPDIR}" ]; then
@@ -173,6 +58,7 @@ example:
 EOF
     exit 1
 }
+check_commands csv_uty.py
 
 while getopts r:h OPT
 do
